25f0a575227980f6871936704e9d2cd22dcfebc0
Julien-Desbard <julien.desbard@gmail.com>
2025-10-13 10:09:39 +0200

    font adjustements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header/header";
 
+import { Playfair_Display, Poppins } from "next/font/google";
+
+const playfair = Playfair_Display({ weight: ["400","500","600","700"], subsets: ["latin"], variable: "--font-title" });
+const poppins  = Poppins({ weight: ["300","400","500","600"], subsets: ["latin"], variable: "--font-body" });
 
 export const metadata: Metadata = {
   title: "le Bar du Centre",
@@ -14,7 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr">
+    <html lang="fr"
+    className={`${playfair.variable} ${poppins.variable}`}>
       <body>
         <Header />
         {children}
--- src/components/homepage/herosection.tsx
+++ src/components/homepage/herosection.tsx
@@ -6,9 +6,9 @@ export default function HeroSection() {
 			id="home"
 			className="snap-start bg-[url('/images/beer.jpg')] bg-cover bg-center h-screen text-center flex items-center max-lg:justify-center"
 		>
-			<div className="px-24 max-lg:flex max-lg:items-center max-lg:flex-col max-sm:px-6">
-				<h1 className="font-title text-5xl pb-12 text-white ">LE BAR DU CENTRE</h1>
-				<h2 className="font-subtitle pb-14 text-2xl text-white">
+			<div className="pt-36 px-24 max-lg:flex max-lg:items-center max-lg:flex-col max-sm:px-6">
+				<h1 className="font-title text-4xl pb-12 font-regular text-white ">LE BAR DU CENTRE</h1>
+				<h2 className="font-body pb-12 text-xl font-thin text-white ">
 					En plein coeur d&apos;Angers, le{" "}
 					<span className="text-secondary">BDC</span> pour les intîmes
 				</h2>
