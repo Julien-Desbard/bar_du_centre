3c651117f56fa1784872100be44adbdedc4b185d
Julien-Desbard <julien.desbard@gmail.com>
2025-10-14 15:15:48 +0200

    menuPage fully responsive

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -5,14 +5,14 @@ export default function MenuSection() {
 	return (
 		<section
 			id="menu"
-			className="grid auto-rows-min justify-center p-6 snap-start relative bg-[url('/images/background.jpg')] bg-cover bg-center min-h-screen text-white
-			lg:grid-cols-2 lg:justify-items-stretch lg:gap-y-6 lg:w-full"
+			className="grid auto-rows-min w-full justify-center p-6 snap-start relative bg-[url('/images/background.jpg')] bg-cover bg-center min-h-screen text-white overflow-x-hidden
+			lg:grid-cols-2 lg:justify-items-stretch lg:gap-y-6"
 		>
-			{/* Menu */}
+			{/* --------------------- Menu ---------------------*/}
 			<div
-				className="grid auto-rows-min mt-12
-			max-sm:max-w-screen max-sm:grid-cols-1 max-sm:gap-12
-			lg:p-6 lg:row-start-1 lg:col-start-1"
+				className="grid auto-rows-min mt-12 justify-self-center
+			max-sm:max-w-full max-sm:grid-cols-1 max-sm:gap-12
+			lg:p-6 lg:row-start-1 lg:col-start-1 lg:mt-24"
 			>
 				<h2 className="col-start-1 justify-self-center self-center font-subtitle text-5xl text-h2 font-semibold">
 					menu de <br />
@@ -51,10 +51,12 @@ export default function MenuSection() {
 					Réserver une table
 				</Button>
 			</div>
-			{/* Carte du jour */}
+			{/* --------------------- Carte du jour ---------------------*/}
+
 			<div
-				className="grid gap-4 auto-rows-min justify-self-center justify-items-center overflow-x-auto max-w-10/12 grid-cols-1
-				lg:mt-18 lg:p-6 lg:col-start-2 lg:max-w-none"
+				className="grid gap-4 auto-rows-min justify-self-center justify-items-center overflow-x-hidden grid-cols-1
+				max-sm:max-w-full
+				lg:mt-30 lg:p-6 lg:col-start-2 lg:max-w-none"
 			>
 				<h2 className="font-subtitle text-5xl text-center pb-6 ">
 					Carte du jour
@@ -121,28 +123,35 @@ export default function MenuSection() {
 					Réserver une table
 				</Button>
 			</div>
-			<Button className="max-w-45 
-			lg:row-start-2 lg:col-span-2 lg:justify-self-center lg:mt-12 lg:mb-18">
+			<Button
+				className="max-w-45 
+			max-lg:hidden
+			lg:row-start-2 lg:col-span-2 lg:justify-self-center lg:mb-6"
+			>
 				Réserver une table
 			</Button>
-			{/* Horaires */}
+			{/* --------------------- Horaires ---------------------*/}
+
 			<div
-				className="grid gap-2 gap-x-4 text-center max-w-screen
+				className="grid gap-2 gap-x-4 text-center max-w-full
 				max-sm:grid-cols-1
 				sm:grid-cols-2 sm:grid-rows-[auto_repeat(3,auto)] sm:grid-flow-col
-				lg:grid-cols-3 lg:grid-rows-[auto_repeat(2,auto)] lg:grid-flow-row lg:p-6 lg:place-self-center lg:justify-items-center lg:row-start-4 lg:col-span-2"
+				lg:grid-cols-3 lg:grid-rows-[auto_repeat(2,auto)] lg:grid-flow-row lg:p-6 lg:place-self-center lg:justify-items-center lg:row-start-4 lg:col-span-2
+				2xl:grid-cols-4"
 			>
 				<h2
 					className="text-4xl font-body justify-self-center pb-4
 				sm:col-span-2 sm:row-start-1
-				lg:col-span-3 "
+				lg:col-span-3
+				2xl:col-start-1 2xl:col-span-1 2xl:row-span-2 2xl:justify-self-center 2xl:self-center
+				"
 				>
 					Les horaires
 				</h2>
 				<ul className="contents">
 					<li
 						className="block justify-self-center self-start text-xl
-					sm:justify-self-start"
+					sm:justify-self-start sm:self-center"
 					>
 						Lundi | Mardi | Mercredi:{" "}
 						<span className="text-secondary">
@@ -152,26 +161,30 @@ export default function MenuSection() {
 					</li>
 					<li
 						className="block justify-self-center self-start text-xl
-					sm:justify-self-start"
+					sm:justify-self-start sm:self-center
+					2xl:self-center"
 					>
 						Jeudi: 12h-14h15 || <span className="text-secondary">19h-23h</span>
 					</li>
 					<li
 						className="block justify-self-center self-start text-xl
-					sm:justify-self-start"
+					sm:justify-self-start sm:self-center
+					2xl:self-center"
 					>
 						Vendredi: 12h-14h30 ||{" "}
 						<span className="text-secondary">19h-23h</span>
 					</li>
 					<li
-						className="block justify-self-center self-start text-xl
-					sm:justify-self-start"
+						className="block justify-self-center self-center text-xl
+					sm:justify-self-start sm:self-center
+					"
 					>
 						Samedi: 12h-15h30 || <span className="text-secondary">19h-23h</span>
 					</li>
 					<li
 						className="block justify-self-center self-start text-xl
-					sm:justify-self-start"
+					sm:justify-self-start sm:row-span-2 sm:self-center
+					lg:row-span-1"
 					>
 						Dimanche: 12h-15h ||{" "}
 						<span className="text-secondary">19h-22h30</span>
